021d090f98f0ca32dd081d93d422d94c42d6966c
antoine picard <antn.picard@gmail.com>
2018-11-13 08:50:24 +0100

    1-initial-situation

--- demo/App.js
+++ demo/App.js
@@ -1,45 +1,16 @@
 import React, { Component } from "react";
-import {
-  Keyboard,
-  ScrollView,
-  StyleSheet,
-  View,
-  TextInput,
-  Image
-} from "react-native";
-import KeyboardSpacer from "react-native-keyboard-spacer";
+import { StyleSheet, View, TextInput, Image } from "react-native";
 
 const image = require("./src/images/bam.png");
 
 export default class App extends Component<Props> {
-  componentDidMount() {
-    this.keyboardDidShowListener = Keyboard.addListener(
-      "keyboardDidShow",
-      this.scroll
-    );
-  }
-  componentWillUnmount() {
-    this.keyboardDidShowListener.remove();
-  }
-  scroll = () => setTimeout(this.scrollView.scrollToEnd, 0);
-
   render() {
     return (
-      <ScrollView ref={component => (this.scrollView = component)}>
-        <View style={styles.pageContainer}>
-          <View style={{ width: "100%" }}>
-            <Image
-              style={styles.imageContainer}
-              resizeMode="center"
-              source={image}
-            />
-          </View>
-          <View style={styles.textInputContainer}>
-            <TextInput placeholder="Ecrivez quelquechose" />
-          </View>
+      <View style={styles.pageContainer}>
+        <View style={styles.textInputContainer}>
+          <TextInput placeholder="Ecrivez quelquechose" />
         </View>
-        <KeyboardSpacer />
-      </ScrollView>
+      </View>
     );
   }
 }
@@ -48,13 +19,6 @@ const styles = StyleSheet.create({
   pageContainer: {
     paddingTop: 20,
     paddingHorizontal: 20
-  },
-  imageContainer: {
-    width: "100%",
-    height: 400
-  },
-  image: {
-    width: "100%"
   },
   textInputContainer: {
     alignItems: "center",
