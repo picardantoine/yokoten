92dc4b41d9d68239c48a3588b10ba0e4a8232c1c
antoine picard <antn.picard@gmail.com>
2018-11-13 08:53:31 +0100

    2-problem

--- demo/App.js
+++ demo/App.js
@@ -7,6 +7,13 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.pageContainer}>
+        <View style={{ width: "100%" }}>
+          <Image
+            style={styles.imageContainer}
+            resizeMode="center"
+            source={image}
+          />
+        </View>
         <View style={styles.textInputContainer}>
           <TextInput placeholder="Ecrivez quelquechose" />
         </View>
@@ -19,6 +26,10 @@ const styles = StyleSheet.create({
   pageContainer: {
     paddingTop: 20,
     paddingHorizontal: 20
+  },
+  imageContainer: {
+    width: "100%",
+    height: 400
   },
   textInputContainer: {
     alignItems: "center",
